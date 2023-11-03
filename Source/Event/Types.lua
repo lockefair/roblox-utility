71d4bbd3ec69b75823411deadfe3efd23820cd1a
Lockefair <joezyth@gmail.com>
2023-11-03 11:50:01 +0000

    Add 'Event' module

---
+++ Source/Event/Types.lua
@@ -0,0 +1,16 @@
+export type EventConnection = {
+	className: string,
+	new: (event: Event) -> EventConnection,
+	Destroy: (self: EventConnection) -> (),
+	Disconnect: (self: EventConnection) -> ()
+}
+
+export type Event = {
+	className: string,
+	new: () -> Event,
+	Destroy: (self: Event) -> (),
+	Connect: (self: Event, callback: (...any) -> ()) -> EventConnection,
+	Fire: (self: Event, ...any) -> ()
+}
+
+return {}
